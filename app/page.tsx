8c8b61db340604b9c3eb99e49dc607e1df6466f5
Andre Costa <andre.andrelscosta@gmail.com>
2026-06-30 21:04:38 -0300

    chore(release): bump PLUGIN_VERSION para v1.6.0

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { HomeLayout } from 'fumadocs-ui/layouts/home';
 import { baseOptions } from '@/lib/layout.shared';
 
-const PLUGIN_VERSION = 'v1.5.1';
+const PLUGIN_VERSION = 'v1.6.0';
 
 export default function Home() {
   return (
